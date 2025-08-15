554c5c72b67eeca3481f2e33919da6802ba81ace
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-08-15 14:43:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggle_btn = document.querySelector('.toggle_btn');
+const toggle_btn_icon = document.querySelector('.toggle_btn i');
+const drop_dwon = document.querySelector('.drop_dwon');
+
+toggle_btn.onclick = () => {
+    drop_dwon.classList.toggle('open');
+    const isOpen = drop_dwon.classList.contains('open');
+ 
+    toggle_btn_icon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
