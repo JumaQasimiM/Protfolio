583baa355796707269bd2135ccb43756695c0ce0
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-08-15 19:00:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ toggle_btn.onclick = () => {
  
     toggle_btn_icon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 }
+// scroll
+window.addEventListener('scroll',()=>{
+const navbar = document.querySelector('.navbar');
+navbar.classList.toggle('scrolled',window.scrollY>50);
+})
+
