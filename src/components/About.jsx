ebe081701fe726a67280f38f13b3c542f38177e9
M Juma <qasimimohammad819@gmail.com>
2025-11-26 13:59:41 +0100

    update About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,51 +9,168 @@ import {
   FaBriefcase,
   FaLanguage,
 } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa6";
+import { TbBrandPython } from "react-icons/tb";
+import { RiTailwindCssLine } from "react-icons/ri";
 import myfoto from "../assets/myfoto.jpg";
 
 export const About = () => {
   const [activeTab, setActiveTab] = useState("education");
 
+  // Educations
   const education = [
     {
-      year: "2015 - 2019",
+      year: "2017 – 2021",
       title: "Bachelor of Computer Science",
-      institute: "University of XYZ",
+      institute: "Polytechnic University Kabul, Afghanistan",
       icon: <FaGraduationCap className="text-blue-500" />,
+      details: (
+        <>
+          <p>
+            <strong>Field of Study:</strong> Computer Engineering & Informatics,
+            Information Sciences
+          </p>
+          <p>
+            <strong>Degree:</strong> Bachelor (Title: Attendance System),
+            recognized in Germany
+          </p>
+          <p>
+            <strong>Project:</strong> Developed an application using Python,
+            Django, and MySQL. Focused on Machine Learning integration.
+          </p>
+        </>
+      ),
     },
     {
-      year: "2020 - 2021",
-      title: "Frontend Development Bootcamp",
-      institute: "ABC Coding Academy",
+      year: "2003 – 2015",
+      title: "School & High School",
+      institute: "Kabul, Afghanistan",
+      icon: <FaGraduationCap className="text-green-500" />,
+    },
+    {
+      year: "2018 - 2019",
+      title: "Full-Stack Development Training – Quality Course",
+      institute: "Synapse Technologies, kabul, Afghanistan",
+      icon: <FaGraduationCap className="text-green-500" />,
+    },
+
+    {
+      year: "03/2025 – 07/2025",
+      title: "Job-Oriented Language Course (B2)",
+      institute: "Volkshochschule Heilbronn, Germany",
       icon: <FaGraduationCap className="text-green-500" />,
     },
   ];
 
+  //  Experince
   const experience = [
+    // Practical / Academic Project
     {
-      year: "2019 - 2020",
-      title: "Junior Front-End Developer",
-      company: "Tech Solutions Ltd",
-      icon: <FaBriefcase className="text-yellow-500" />,
+      year: "2020 – 2021",
+      title: "Bachelor Project: Attendance System",
+      company: "Polytechnic University Kabul, Afghanistan",
+      icon: <FaBriefcase className="text-blue-500" />,
+      details: (
+        <>
+          <p>Full-Stack Development: Frontend + Backend</p>
+          <p>Technologies: Python, Django, MySQL, OpenCV, FaceNet</p>
+          <p>Focus: Machine Learning integration with face recognition</p>
+        </>
+      ),
+    },
+
+    // Personal / Practice Projects
+    {
+      year: "2024 – Present",
+      title: "React Shop",
+      company: "Personal Project",
+      icon: <FaBriefcase className="text-green-500" />,
+      details: (
+        <>
+          <h5 className="text-teal-700 font-semibold">Personal Project</h5>
+          <p>A responsive E-commerce website using React and TailwindCSS</p>
+          <p>Technologies: React, TailwindCSS, SwiperJS, REST API</p>
+          <p>
+            Live Demo:{" "}
+            <a
+              href="https://jumaqasimim.github.io/Tailwindshop/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline hover:text-blue-700"
+            >
+              Shop
+            </a>
+          </p>
+        </>
+      ),
     },
     {
-      year: "2021 - Present",
-      title: "Front-End Developer",
-      company: "Creative Web Agency",
-      icon: <FaBriefcase className="text-red-500" />,
+      year: "2025 – 06/2025",
+      title: "JavaScript InfoPlus24",
+      company: "Practice Project",
+      icon: <FaBriefcase className="text-yellow-500" />,
+      details: (
+        <>
+          <h5 className="text-teal-700 font-semibold">Personal Project</h5>
+          <p>A news website built with vanilla JavaScript</p>
+          <p>Technologies: JavaScript, HTML, TailwindCSS, REST API</p>
+          <p>
+            Live Demo:{" "}
+            <a
+              href="https://jumaqasimim.github.io/InfoPlus24/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline hover:text-blue-700"
+            >
+              InfoPlus24
+            </a>
+          </p>
+        </>
+      ),
+    },
+    {
+      year: "2025 – Present",
+      title: "React CodeManager",
+      company: "Personal Project",
+      icon: <FaBriefcase className="text-purple-500" />,
+      details: (
+        <>
+          <h5 className="text-teal-700 font-semibold">Personal Project</h5>
+          <p>A CodeManager dashboard to save codes, styles, and links</p>
+          <p>Technologies: React, TailwindCSS, HTML5</p>
+          <p>
+            GitHub :{" "}
+            <a
+              href="https://github.com/JumaQasimiM/CodeManager"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline hover:text-blue-700"
+            >
+              CodeManager
+            </a>
+          </p>
+        </>
+      ),
     },
   ];
 
   const skills = [
+    {
+      name: "Python",
+      icon: <TbBrandPython className="text-blue-700 " />,
+    },
+    { name: "JavaScript", icon: <FaJsSquare className="text-yellow-400" /> },
     { name: "React", icon: <FaReact className="text-blue-500" /> },
     { name: "HTML5", icon: <FaHtml5 className="text-orange-500" /> },
     { name: "CSS3", icon: <FaCss3Alt className="text-blue-600" /> },
-    { name: "JavaScript", icon: <FaJsSquare className="text-yellow-400" /> },
-    { name: "TailwindCSS", icon: null },
-    { name: "Git & GitHub", icon: null },
+    {
+      name: "TailwindCSS",
+      icon: <RiTailwindCssLine className="text-sky-700" />,
+    },
+    { name: "Git & GitHub", icon: <FaGitAlt className="text-gary-600" /> },
   ];
 
-  const languages = ["Deutsch", "English", "Persisch"];
+  const languages = ["Deutsch", "English", "Persisch (Mother Language)"];
   const tabs = ["education", "experience", "skills", "languages"];
 
   return (
@@ -130,52 +247,71 @@ export const About = () => {
           <div className="space-y-3 sm:space-y-4 md:space-y-6">
             {/* Education */}
             {activeTab === "education" &&
-              education.map((edu, index) => (
+              education.map((edu, i) => (
                 <motion.div
-                  key={index}
-                  initial={{ opacity: 0, y: 20 }}
-                  whileInView={{ opacity: 1, y: 0 }}
+                  key={i}
+                  initial={{ opacity: 0, x: -20 }}
+                  whileInView={{ opacity: 1, x: 0 }}
                   viewport={{ once: true }}
-                  transition={{ duration: 0.5, delay: index * 0.1 }}
-                  className="flex flex-col sm:flex-row items-start sm:items-center gap-3 sm:gap-4 p-3 sm:p-4 rounded-xl bg-white shadow-md border border-gray-200 hover:scale-105 transition"
+                  transition={{ duration: 0.5, delay: i * 0.1 }}
+                  className="relative mb-6"
                 >
-                  <div className="text-2xl sm:text-3xl">{edu.icon}</div>
-                  <div>
-                    <h3 className="font-semibold text-gray-900 text-sm sm:text-base md:text-lg">
-                      {edu.title}
-                    </h3>
-                    <p className="text-gray-700 text-xs sm:text-sm md:text-base">
+                  <span className="absolute -left-[16px] top-2 w-8 h-8 flex items-center justify-center bg-white border-2 border-blue-400 rounded-full shadow">
+                    {edu.icon}
+                  </span>
+                  <div className="bg-white p-5 rounded-xl shadow-md border border-gray-200 hover:shadow-lg transition">
+                    <div className="flex justify-between items-center mb-1">
+                      <h3 className="text-xl font-bold text-gray-900">
+                        {edu.title}
+                      </h3>
+                      <span className="text-sm text-blue-500 font-semibold">
+                        {edu.year}
+                      </span>
+                    </div>
+                    <p className="text-gray-700 font-medium mb-2">
                       {edu.institute}
                     </p>
-                    <span className="text-gray-400 text-xs sm:text-xs md:text-sm">
-                      {edu.year}
-                    </span>
+                    {edu.details && (
+                      <div className="text-gray-600 text-sm space-y-1">
+                        {edu.details}
+                      </div>
+                    )}
                   </div>
                 </motion.div>
               ))}
 
             {/* Experience */}
+
             {activeTab === "experience" &&
-              experience.map((exp, index) => (
+              experience.map((exp, i) => (
                 <motion.div
-                  key={index}
-                  initial={{ opacity: 0, y: 20 }}
-                  whileInView={{ opacity: 1, y: 0 }}
+                  key={i}
+                  initial={{ opacity: 0, x: -20 }}
+                  whileInView={{ opacity: 1, x: 0 }}
                   viewport={{ once: true }}
-                  transition={{ duration: 0.5, delay: index * 0.1 }}
-                  className="flex flex-col sm:flex-row items-start sm:items-center gap-3 sm:gap-4 p-3 sm:p-4 rounded-xl bg-white shadow-md border border-gray-200 hover:scale-105 transition"
+                  transition={{ duration: 0.5, delay: i * 0.1 }}
+                  className="relative mb-6"
                 >
-                  <div className="text-2xl sm:text-3xl">{exp.icon}</div>
-                  <div>
-                    <h3 className="font-semibold text-gray-900 text-sm sm:text-base md:text-lg">
-                      {exp.title}
-                    </h3>
-                    <p className="text-gray-700 text-xs sm:text-sm md:text-base">
-                      {exp.company}
+                  <span className="absolute -left-[16px] top-2 w-8 h-8 flex items-center justify-center bg-white border-2 border-blue-400 rounded-full shadow">
+                    {exp.icon}
+                  </span>
+                  <div className="bg-white p-5 rounded-xl shadow-md border border-gray-200 hover:shadow-lg transition">
+                    <div className="flex justify-between items-center mb-1">
+                      <h3 className="text-xl font-bold text-gray-900">
+                        {exp.title}
+                      </h3>
+                      <span className="text-sm text-blue-500 font-semibold">
+                        {exp.year}
+                      </span>
+                    </div>
+                    <p className="text-gray-700 font-medium mb-2">
+                      {exp.institute}
                     </p>
-                    <span className="text-gray-400 text-xs sm:text-xs md:text-sm">
-                      {exp.year}
-                    </span>
+                    {exp.details && (
+                      <div className="text-gray-600 text-sm space-y-1">
+                        {exp.details}
+                      </div>
+                    )}
                   </div>
                 </motion.div>
               ))}
